54f826510b7cf0b7b4e19806bde27861b6529855
Manohar-mata <matamanohar612@gmail.com>
2025-01-07 11:33:10 +0530

    data validation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.Entity.config_entity import DataIngestionConfig
+from networksecurity.Entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.Entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
 import sys
 
 if __name__=="__main__":
@@ -12,8 +13,15 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info(f"initiate the data ingestion")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
-        logging.info("Data Ingestion is completed")
+        logging.info(f"Data Ingestion is completed")
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info(f"initiate data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info(f"data validation is completed")
+        print(data_validation_artifact)
+        
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/Entity/config_entity.py
+++ networksecurity/Entity/config_entity.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 import os
 
-from networksecurity.constant.training_pipeline import __inti__
+from networksecurity.constant import training_pipeline
 
 
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
-        training_pipeline=__inti__
         timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name=training_pipeline.PIPELINE_NAME
         self.artifact_name=training_pipeline.ARTIFACT_DIR
@@ -19,7 +18,6 @@ class TrainingPipelineConfig:
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-        training_pipeline=__inti__
         self.data_ingestion_dir: str=os.path.join(
             training_pipeline_config.artifacts_dir,training_pipeline.DATA_INGESTION_INGESTED_DIR
             )
@@ -35,3 +33,29 @@ class DataIngestionConfig:
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir: str=os.path.join(
+            training_pipeline_config.artifact_name,training_pipeline.DATA_INGESTION_DIR_NAME
+        )
+        self.valid_data_dir: str=os.path.join(
+            self.data_validation_dir,training_pipeline.DATA_VALIDATION_VALID_DIR
+        )
+        self.invalid_data_dir: str=os.path.join(
+            self.data_validation_dir,training_pipeline.DATA_VALIDATION_INVALID_DIR
+        )
+        self.valid_train_file_path=os.path.join(
+            self.data_validation_dir,training_pipeline.TRAIN_FILE_NAME
+        )
+        self.valid_test_file_path=os.path.join(
+            self.data_validation_dir,training_pipeline.TEST_FILE_NAME
+        )
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        )
+
+        
